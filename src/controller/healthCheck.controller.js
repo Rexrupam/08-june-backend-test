18acb0bc7b76611c117f828bb18478d378f0cf93
rupam <rexrupam70@gmail.com>
2025-06-08 13:43:14 +0530

    add an npm package

--- src/controller/healthCheck.controller.js
+++
@@ -1,3 +0,0 @@
-export const healthCheck=async(req,res)=>{
-    return res.status(200).json({message: 'Ok'})
-}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-import { app } from "./app.js"
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+#!/usr/bin/env node
 
-dotenv.config({
-    path: './.env'
-})
+console.log(`
+Rupam Bhattacharyya's resume
+----------------------------
+Backend Developer | Node.js | MongoDB | Express
 
+GitHub: https://github.com/Rexrupam
+LinkedIn: https://www.linkedin.com/in/rexrupam
+Portfolio: https://rexrupam-github-io.onrender.com/portfolio.html
+Email: rexrupam70@gmail.com
 
-connectDB()
-.then(()=>{
-    app.listen(process.env.port || 8000, ()=>{
-        console.log(`Server is listening on port: ${process.env.port}`)
-    })
-    
-})
-.catch((err)=>{
-    console.log('MongoDB connection failed',err)
-})
+Experience:
+ -  https://github.com/Rexrupam/videotube
+    - This is a backend of a Youtube-like video streaming platform built with Node.js, Express, and MongoDB.
+    - It includes features like user authentication, video upload, and streaming.
+    - It uses JWT for authentication and Multer for file uploads.
+    - It uses cloudinary for image and video storage.
+    - aggregation pipeline is used for complex queries.
+ -  https://github.com/Rexrupam/mini-youtube-dashboard-api
+    - Using youtube data API, this project display title and description of a video.
+    - It uses Node.js, Express, and MongoDB.
+    - Oauth2.0 is used for authentication.
+    - user can comment through this API and delete their comment.
+    - A database log is maintained to track the user activity.
+    - User can left a note for improvements of the video.
+ -  https://github.com/Rexrupam/spotify-activity-api
+    - Oauth2.0 is used for authentication.
+    - User can get their current playing song, top tracks.
+    - User can pause, resume current playing song.
+ -  https://github.com/Rexrupam/book-my-show
+    - User can book a movie ticket.
+    - It can fetch last booking details. 
+ - https://github.com/Rexrupam/git-activity-api
+    - Get GitHub user details, including followers and following count.
+    - Fetch information about a specific repository.
+    - Create an issue in a GitHub repository and return the issue URL.
+   `)
